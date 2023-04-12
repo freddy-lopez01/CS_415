0b39af0e9f9d9ce0cbd81b3d7d06976a2f004afe
Freddy Lopez <freddlopez01@gmail.com>
2023-04-12 07:47:55 -0700

    pushing semi-completed hashset implementation files

--- hashset.c
+++ hashset.c
@@ -1,10 +1,28 @@
 #include "hashset.h"  /* the .h file does NOT reside in /usr/local/include/ADTs */
 #include <stdlib.h>
+#include <stdio.h>
+
 /* any other includes needed by your code */
 #define UNUSED __attribute__((unused))
+#define SHIFT 31L 
+
+// added node structure 
+typedef struct node {
+    struct node *next;
+} Node; 
+
+
 
 typedef struct s_data {
     /* definitions of the data members of self */
+    void (*freeValue)(void *e);
+    int (*cmpFxn) (void *first, void *second);
+    long changes; //recommended by the book
+    long capacity; 
+    double loadFactor;
+    long (*hashFxn) (void*, long);
+    Node **buckets; 
+
 } SData;
 
 /*
@@ -12,29 +30,81 @@ typedef struct s_data {
  * methods
  */
 
-static void s_destroy(UNUSED const Set *s) {
+// Helper functions recommended by the Book 
+
+static void purge(SData *st){
+    long i; 
+    Node *p;
+    for(i = 0L; i < st->capacity; i++){ 
+        p = st->buckets[i];
+        while(p != NULL){
+            st->freeValue(st->buckets[i]);
+        }
+        st->buckets[i] = NULL;
+    }
+}
+
+
+// Flesh these functions out
+
+static void s_destroy(const Set *s) {
     /* implement the destroy() method */
-    return;
+    SData *st = (SData *)s->self; 
+
+    purge(st);
+    free(st->buckets);
+    free(st);
+    free((void*)st); // this may not be needed but read book tommorow 04/16/23
 }
 
-static void s_clear(UNUSED const Set *s) {
+static void s_clear(const Set *s) {
     /* implement the clear() method */
-    return;
+    SData *st = (SData*)s->self; 
+
+    purge(st);
+    st->capacity = 0;
+    st->loadFactor = 0.0;
+    st->changes = 0; 
+    //s_destroy(s);
+                    // This needs to be checked once I go back through because I dont belive this
+                    // is entirely the correct way to go about this
 }
 
-static bool s_add(UNUSED const Set *s, UNUSED void *member) {
-    /* implement the add() method */
-    return 0;
+static bool s_add(const Set *s, UNUSED void *member) {
+    /* implement the add() method 
+    SData *st = (SData*)s->self; 
+    if(s_contains(st, member)){
+        return false; 
+    }else{
+        hashFxn(member, st->capacity);
+    } */
+    return true;
 }
 
-static bool s_contains(UNUSED const Set *s, UNUSED void *member) {
+static Node *findMember(SData *st, void *member, long *bucket){
+    long i = hashFxn(member, st->capacity);
+    Node *p; 
+    for(p = st->buckets[i]; p != NULL; i++){
+        if(cmpFxn(p, member) == 0){
+            break;
+        }
+    }
+    return p;
+}
+
+
+static bool s_contains(const Set *s, void *member) {
     /* implement the contains() method */
-    return 0;
+    SData *st = (SData*)s->self;
+    long bucket;
+
+    return (findMember(st, member, &bucket) != NULL); 
 }
 
-static bool s_isEmpty(UNUSED const Set *s) {
+static bool s_isEmpty(const Set *s) {
     /* implement the isEmpty() method */
-    return 1;
+    SData *st = (SData*)s->self;
+    return (st->capacity == 0L); // check if st->next is equivalent to 0L. If it is then true, else false
 }
 
 static bool s_remove(UNUSED const Set *s, UNUSED void *member) {
@@ -42,9 +112,10 @@ static bool s_remove(UNUSED const Set *s, UNUSED void *member) {
     return 0;
 }
 
-static long s_size(UNUSED const Set *s) {
+static long s_size(const Set *s) {
     /* implement the size() method */
-    return 0L;
+    SData *st = (SData*)s->self; 
+    return st->capacity;
 }
 
 static void **s_toArray(UNUSED const Set *s, UNUSED long *len) {
@@ -57,15 +128,17 @@ static const Iterator *s_itCreate(UNUSED const Set *s) {
     return NULL;
 }
 
-static UNUSED Set template = {
+static Set template = {
     NULL, s_destroy, s_clear, s_add, s_contains, s_isEmpty, s_remove,
     s_size, s_toArray, s_itCreate
 };
 
-const Set *HashSet(UNUSED void (*freeValue)(void*), UNUSED int (*cmpFxn)(void*, void*),
-                   UNUSED long capacity, UNUSED double loadFactor,
-                   UNUSED long (*hashFxn)(void *m, long N)
+const Set *HashSet(void (*freeValue)(void*), int (*cmpFxn)(void*, void*),
+                   long capacity, double loadFactor,
+                    long (*hashFxn)(void *m, long N)
                   ) {
     /* construct a Set instance and return to the caller */
-    return NULL;
+    SData *s = (SData*)malloc(sizeof(capacity));
+
+    return s;
 }
